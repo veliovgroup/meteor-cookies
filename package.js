5e0579746ee2758faaa7bf26ac7496c4ecd30f23
dr.dimitru <ceo@veliov.com>
2017-05-16 23:43:52 +0300

    v2.2.1
     - Compatibility with `meteor@1.4.4.2`
     - Meteor dependencies update
     - Minor codebase enhancements

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:cookies',
-  version: '2.2.0',
+  version: '2.2.1',
   summary: 'Isomorphic bulletproof Server and Client cookie functions',
   git: 'https://github.com/VeliovGroup/Meteor-Cookies',
   documentation: 'README.md'
