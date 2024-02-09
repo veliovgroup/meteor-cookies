b9c283af4f69c14d49ac80ef770da6680ec0eb32
dr.dimitru <ceo@veliov.com>
2024-02-10 00:52:10 +0200

    📦 v2.8.0
    ☄️ Packosphere [`@2.8.0`](https://packosphere.com/ostrio/cookies)
    
    __Changes:__
    
    - 🖥️ Meteor-Desktop support, thanks to @dd137
    - 🚀 Support for `meteor@3.0-beta.0`, thanks to @dallman2
    - 📔 Docs update to match changes
    
    __Dependencies__:
    
    - 📦 `fetch@0.1.4`, *was `v0.1.1`*

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'ostrio:cookies',
-  version: '2.7.2',
+  version: '2.8.0',
   summary: 'Isomorphic bulletproof Server, Client, Browser, and Cordova cookies',
   git: 'https://github.com/veliovgroup/Meteor-Cookies',
   documentation: 'README.md'
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('1.4');
+  api.versionsFrom(['1.4', '3.0-beta.0']);
   api.use('ecmascript', ['client', 'server']);
   api.use('webapp', 'server');
-  api.use('fetch@0.1.1', 'client');
+  api.use('fetch@0.1.4', 'client');
   api.mainModule('cookies.js', ['client', 'server']);
 });
 
