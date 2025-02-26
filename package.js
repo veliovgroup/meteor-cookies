de22113feec2756b89df039d887485e1627a1548
dr.dimitru <ceo@veliov.com>
2025-02-26 12:44:21 +0200

    📦 v2.9.0
    - 🤝 Compatibility with `meteor@3`
    - 🤓 TypeScript definitions
    - 🤓 100% JSDoc coverage
    - 📔 Improved documentation with more examples
    - 👨‍🔬 Refactored and improved test-suite, covering 99.9% of codebase and usage cases
    - ✨ Support latest cookies options (`partitioned`, `priority`)
    - 👨‍💻 Replaced deprecated Browser and Node.js methods and functions
    - 👨‍💻 Optimized codebase for performance

--- package.js
+++ package.js
@@ -1,23 +1,30 @@
 Package.describe({
   name: 'ostrio:cookies',
-  version: '2.8.1',
+  version: '2.9.0',
   summary: 'Isomorphic bulletproof Server, Client, Browser, and Cordova cookies',
   git: 'https://github.com/veliovgroup/Meteor-Cookies',
   documentation: 'README.md'
 });
 
 Package.onUse((api) => {
-  api.versionsFrom(['1.4', '3.0-beta.0']);
+  api.versionsFrom(['1.4', '3.0.1']);
   api.use('ecmascript', ['client', 'server']);
   api.use('webapp', 'server');
-  api.use('fetch@0.1.4 || 0.1.4-beta300.0', 'client');
+  api.use('fetch@0.1.5', 'client');
   api.mainModule('cookies.js', ['client', 'server']);
+
+  // TypeScript setup
+  api.use(['zodern:types@1.0.13', 'typescript'], ['client', 'server'], { weak: true });
+  // For zodern:types to pick up our published types.
+  api.addAssets('index.d.ts', ['client', 'server']);
 });
 
 Package.onTest((api) => {
   api.use('blaze-html-templates');
   api.use('tinytest');
-  api.use(['ecmascript', 'http'], ['client', 'server']);
+  api.use(['ecmascript', 'http', 'ostrio:cookies@2.9.0'], ['client', 'server']);
   api.use(['ejson', 'webapp'], 'server');
-  api.addFiles('cookies-tests.js', ['client', 'server']);
+  api.addFiles('tests/both.js', ['client', 'server']);
+  api.addFiles('tests/server.js', 'server');
+  api.addFiles('tests/client.js', 'client');
 });
